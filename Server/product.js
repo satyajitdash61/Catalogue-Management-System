387d38af7ce698796c69225b046b9a9a02ecd43e
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-12-17 17:58:55 +0530

    saving in mongodb

--- Server/product.js
+++ Server/product.js
@@ -1,11 +1,30 @@
 const express=require('express');
 const router=express.Router();
 const mongoose=require('mongoose');
-const User =require("./Model/productmodel");
+const Product =require("./Model/productmodel");
 
 
 router.post('/productinfo',(req,res,next) => {
-    console.log("here");
+    // console.log("here");
+    const {name,brand,category,parent,breadcrumb,specifications} = req.body;
+    // console.log(specifications);
+    const product = new Product({
+        name:req.body.name,
+        brand:req.body.brand,
+        category:req.body.category,
+        parent:req.body.parent,
+        breadcrumb:req.body.breadcrumb,
+        specifications:req.body.specifications,
+
+    });
+    
+    console.log(product);
+    product.save()
+    .then(product=>{
+        res.status("200").send("Success");
+    })
+    .catch(err=>console.log(err)
+    )
     
 })
 
