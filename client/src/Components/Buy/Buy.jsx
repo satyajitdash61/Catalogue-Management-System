ad69b921d8070c8323531e5c094c603c8893d625
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-12-19 09:31:33 +0530

    Update Buy.jsx

--- client/src/Components/Buy/Buy.jsx
+++ client/src/Components/Buy/Buy.jsx
@@ -1,14 +1,47 @@
-import React, { Component } from 'react';
-import './Buy.css'
+import React, { Component } from "react";
+import "./Buy.css";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 class Buy extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                <h2>This is buy page</h2>
-            </div>
-         );
-    }
+  constructor() {
+    super();
+    this.state = {
+      res: []
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:8300/product/buy")
+      .then(response => response.json())
+      .then(value => {
+        console.log(value[0].name);
+        this.setState({
+          res: value
+        });
+      });
+  }
+  render() {
+    const { res } = this.state;
+    return (
+      <div>
+        {res.map(res => (
+           <Card variant="mr-3">
+           <Card.Header as="h3">{res["name"]}</Card.Header>
+           <Card.Body>
+             <Card.Title>{res["brand"]}</Card.Title>
+             <Card.Text>
+               <p> Category : {res["category"]}</p>
+               <p> Parent Company : {res["parent"]} </p>
+               <p> Breadcrumb : {res["breadcrumb"]}</p>
+               <p> Specifications : {res["specifications"]}</p>
+             </Card.Text>
+           </Card.Body>
+         </Card>
+        ))}
+      </div>
+    );
+  }
 }
- 
+
 export default Buy;
